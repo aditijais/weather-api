7ba0fb3f201bdf2fd8da35e6751123b1a54aaeb2
Aditi <aditijaiswal275@gmail.com>
2023-01-18 20:12:29 +0530

    Api called

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import axios from 'axios';
 
 
 function Navbar() {
@@ -8,32 +9,27 @@ function Navbar() {
   const inputEvent=(e)=>{
     setLocation(e.target.value);
   }
+
    const handleonsubmit=(e)=>{
       e.preventDefault();
       console.log(location);
-      fetch(`https://api.weatherapi.com/v1/current.json?key=970b42fd54bd40d69e7120153231101&q=DURGAPUR&aqi=no`)
-      .then(Response=>Response.json())
-      .then((data)=>{
-    console.log(data);
-    setWeather(data.result[0]);
-      })
+      axios.get(`https://api.weatherapi.com/v1/current.json?key=970b42fd54bd40d69e7120153231101&q=${location}&aqi=no`)
+      
+      .then(data => data.data)
+      .then(data=>setWeather(data.current))
+      console.log(weather)
    } 
-    // const getUsers= async() => {
-    //     const response= await fetch(`https://codeforces.com/api/user.info?handles=${handle}`);
-    //     setUser(await response.json());
-    //     // console.log("Hello");
-    //     // console.log(data);
-    //     //const maxRating= await data.result[0].maxRating;
-    //     //console.log(maxRating);
-
-    // }
+  
     
     
 
   return (
     <>
-    <div>
-    <img src="/images/weatherapiLogo.jpg" alt=" " className="cf--logo"/>
+    <div className="nav--head">
+    <img src="/images/weather-logo.png" alt=" " className="cf--logo"/>
+    <h1 className="weather--temp">{weather.temp_c} °C </h1>
+    <div>{weather.temp_c<25 && <img src="/images/summer.jpg" alt=" "/>}</div>
+    {weather.last_updated}
     </div>
     <div className="nav--search">
       <input type='text' placeholder='Enter Location...' className="input--search"
@@ -42,13 +38,12 @@ function Navbar() {
       <button onClick={handleonsubmit}>Search</button>
       
     </div>
-    <div className="users">
-    <userinfo className="user--info">
-       <p>{weather.result[0]}</p>
-       {location}
-    </userinfo>
     
-    </div>
+      <div></div>
+      
+      <div className="feels--like">Feels like : {weather.feelslike_c}</div>
+      
+    
     </>
   );
 }
